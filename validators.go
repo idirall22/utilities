e369da64d434f2af4cc85ad467776cdc7826eb3a
idir <idirall22@gmail.com>
2019-10-23 22:02:54 +0100

    added validators.go

---
+++ validators.go
@@ -0,0 +1,27 @@
+package utilities
+
+import "math"
+
+// ValidateID validate an id used to query
+func ValidateID(id int64) error {
+	if id < 0 || id > math.MaxInt64 {
+		return ErrorID
+	}
+	return nil
+}
+
+// ValidateLimitAndOffset validate offset and limit used for query
+func ValidateLimitAndOffset(off, lim, defaultLimit, maxLimit int) (int, int) {
+
+	if lim < defaultLimit {
+		lim = defaultLimit
+	} else if lim > maxLimit {
+		lim = maxLimit
+	}
+
+	if off < 0 {
+		off = 0
+	}
+
+	return off, lim
+}
