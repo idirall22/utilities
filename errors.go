4016bb1bf59c7d7878047eb751f5bec312fa2306
idir <idirall22@gmail.com>
2019-11-05 19:43:57 +0100

    Added errors utilities

--- errors.go
+++ errors.go
@@ -5,4 +5,7 @@ import "errors"
 var (
 	// ErrorID when id is not valid
 	ErrorID = errors.New("Id not valid")
+
+	// ErrorUserID when user id in context is not valid
+	ErrorUserID = errors.New("User Unauthorized")
 )
